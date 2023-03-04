6d2198a6d4cc15bb73ae8b1684248bf54e2848e1
RaoAsim <80324350+RaoAsim@users.noreply.github.com>
2023-03-05 01:24:53 +0500

    new changes

---
+++ src/components/App.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useRef } from 'react'
+import {  SwapWidget,Theme } from '@uniswap/widgets'
+
+// ↓↓↓ Don't forget to import the widgets' fonts! ↓↓↓
+import '@uniswap/widgets/fonts.css'
+// ↑↑↑
+
+import { useActiveProvider } from '../connectors'
+import { JSON_RPC_URL } from '../constants'
+import bg from '../assests/bg.png'
+import logo from '../assests/R.jpg'
+import Web3Connectors from './Web3Connectors'
+import styles from '../styles/Home.module.css'
+
+const TOKEN_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
+const UNI = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
+
+export default function App() {
+
+  const connectors = useRef<HTMLDivElement>(null)
+  const focusConnectors = useCallback(() => connectors.current?.focus(), [])
+  const theme: Theme = {
+    primary: '#FFF',
+    secondary: '#A9A9A9',
+    interactive: '#A9A9A9',
+    container: '#4E4E5A',
+    module: '#222633',
+    accent: '#71FF98',
+    outline: '#CC1',
+    dialog: '#000',
+    fontFamily: 'Josefin Sans',
+    borderRadius: 0.5,
+  }
+  const provider = useActiveProvider()
+
+  return (
+    <div className={styles.container}  style={{backgroundImage:`url(${bg})`}}>
+      <div className={styles.i18n}>
+      <img src={logo} height="50px" width={"50px"} style={{borderRadius:"8px"}}/>
+        <Web3Connectors />
+      </div>
+      <main className={styles.main}>
+        <div className={styles.demo}>
+          <div className={styles.widget}>
+            <SwapWidget
+            theme={theme}
+              jsonRpcEndpoint={JSON_RPC_URL}
+              tokenList={TOKEN_LIST}
+              provider={provider}
+              onConnectWallet={focusConnectors}
+              defaultInputTokenAddress="NATIVE"
+              defaultInputAmount="1"
+              defaultOutputTokenAddress={UNI}
+            />
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/components/Web3Connectors.tsx
@@ -0,0 +1,35 @@
+import styles from '../styles/Connectors.module.css'
+import stylesHome from '../styles/Home.module.css'
+import { connectors, getConnectorName, Web3Connector } from '../connectors'
+import { useCallback } from 'react'
+
+function Connector({ web3Connector }: { web3Connector: Web3Connector }) {
+  const [connector, hooks] = web3Connector
+  const isActive = hooks.useIsActive()
+  const onClick = useCallback(() => {
+    if (isActive) {
+      connector.deactivate()
+    } else {
+      connectors.forEach(([connector]) => connector.deactivate())
+      connector.activate()
+    }
+  }, [connector, isActive])
+
+  return (
+    <div >
+      
+      <button onClick={onClick} className={stylesHome.btnconnect}>{isActive ? 'Disconnect' : 'Connect'}</button>
+      
+    </div>
+  )
+}
+
+export default function Connectors() {
+  return (
+ <>
+      {connectors.map((web3Connector, index) => (
+        <Connector key={index} web3Connector={web3Connector} />
+      ))}
+ </>
+  )
+}
